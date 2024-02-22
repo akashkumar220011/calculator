5267365110cb57151eac36a6239b04098403d31f
akashkumar220011 <80808362+akashkumar220011@users.noreply.github.com>
2024-02-22 15:24:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [value, setValue] = useState('');
+
+// creating a function
+const handleButtonClick = (btnValue)=> {
+  if( btnValue === '='){
+    try{
+      setValue(eval(value).toString());
+    }
+    catch(error){
+      setValue('Error');
+    }
+  }
+  else if(btnValue === 'AC'){
+    setValue('');
+  }
+  else if(btnValue=== 'DE'){
+    setValue(value.slice(0,-1));
+  }
+  else{
+    setValue(value + btnValue);
+  }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+      <div className='calculator'>
+        <form action="">
+          <div className='display'>
+            <input type="text" value={value} readOnly/>
+          </div>
+          <div className='btn'>
+            <input type='button' value="AC" onClick={() => handleButtonClick('AC')}/>
+            <input type='button' value="DE" onClick={() => handleButtonClick('DE')}/>
+            <input type='button' value="." onClick={() => handleButtonClick('.')}/>
+            <input type='button' value="/" onClick={() => handleButtonClick('/')}/>
+          </div >
+          <div className='btn'>
+          <input type='button' value='7' onClick={() => handleButtonClick('7')} />
+          <input type='button' value='8' onClick={() => handleButtonClick('8')} />
+          <input type='button' value='9' onClick={() => handleButtonClick('9')} />
+          <input type='button' value='*' onClick={() => handleButtonClick('*')} />
+        </div>
+        <div className='btn'>
+          <input type='button' value='4' onClick={() => handleButtonClick('4')} />
+          <input type='button' value='5' onClick={() => handleButtonClick('5')} />
+          <input type='button' value='6' onClick={() => handleButtonClick('6')} />
+          <input type='button' value='+' onClick={() => handleButtonClick('+')} />
+        </div>
+        <div className='btn'>
+          <input type='button' value='1' onClick={() => handleButtonClick('1')} />
+          <input type='button' value='2' onClick={() => handleButtonClick('2')} />
+          <input type='button' value='3' onClick={() => handleButtonClick('3')} />
+          <input type='button' value='-' onClick={() => handleButtonClick('-')} />
+        </div>
+        <div className='btn'>
+          <input type='button' value='00' onClick={() => handleButtonClick('00')} />
+          <input type='button' value='0' onClick={() => handleButtonClick('0')} />
+          <input type='button' value='=' onClick={() => handleButtonClick('=')} />
+        </div>
+        </form>
+      </div>
+   </div>
   );
 }
 
